dbb2ebd71a49b6f516fb5b0109b6828837c8e5c7
Jeff Jin <mychinook@me.com>
2013-09-23 23:56:32 -0400

    admin page list and edit page added

--- client/app/js/directives/markdown-editor.js
+++ client/app/js/directives/markdown-editor.js
@@ -4,16 +4,19 @@
 
 
 (function(module){
-  module.directive('wmdEditor', ['$window', function($window) {
+  module.directive('markdownEditor', ['$window', function($window) {
 
     return {
       restrict: 'EA',
       replace: true, // The element containing the directive will be replaced with the template
       templateUrl: './js/directives/templates/markdown-editor.html',
       scope: {
+        content: '='
       },
       link: function (scope, element, attrs) {
-
+        var converter = Markdown.getSanitizingConverter();
+        var editor = new Markdown.Editor(converter);
+        editor.run();
       }
     };
   }]);
