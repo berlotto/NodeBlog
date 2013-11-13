be29e6dbc3485ec385ca2f8bc17055b18d0827ba
Jeff Jin <Jeff@JeffJin.com>
2013-11-13 00:38:50 -0500

    custom http interceptor added. To be configured to intercept failed ajax calls

--- client/app/bootstrap.js
+++ client/app/bootstrap.js
@@ -3,9 +3,10 @@
 (function(exports){
    exports.FilterModule = angular.module('chinook.filters', []);
    exports.ServiceModule = angular.module('chinook.services', []);
+   exports.FactoryModule = angular.module('chinook.factories', []);
    exports.DirectiveModule = angular.module('chinook.directives', []);
    exports.CtrlModule = angular.module('chinook.controllers', []);
    exports.MainModule = angular.module('chinook',
-       ['chinook.filters', 'chinook.services', 'chinook.directives', 'chinook.controllers']);
+       ['chinook.filters', 'chinook.services', 'chinook.factories', 'chinook.directives', 'chinook.controllers']);
 
 })(window);
