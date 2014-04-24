cf7fba91bf08e7d812c7ed67de79c3cad563e094
jeff jin <jeff@jeffjin.com>
2014-04-24 01:43:36 -0400

    continuous image load added(not finished)

--- app/scripts/directives/jj-image-slider.js
+++ app/scripts/directives/jj-image-slider.js
@@ -9,11 +9,26 @@
             controller: 'ImageListCtrl',
             link: function($scope, element, attrs){
                console.log('cnkImageSlider link is called');
-               var pageIndex = 0, pageSize = 30, currentSlideIndex = 0;
+               var pageIndex = 0, pageSize = 15, currentSlideIndex = 0;
                function slideContentChange(args) {
                   /* indicator */
                   currentSlideIndex = args.currentSlideNumber - 1;
-                  console.log('currentSlideIndex', currentSlideIndex);
+                  //console.log('args', args);
+                  //increasing direction
+                  if(currentSlideIndex >= (pageSize - 2) && args.targetSlideNumber > args.prevSlideNumber){
+                     console.log('currentSlideIndex', currentSlideIndex);
+                     pageIndex++;
+                     console.log('pageIndex', pageIndex);
+                     imageService.getList(pageIndex, pageSize)
+                        .then(function(result) {
+                           $scope.images = $scope.images.concat(result.data);
+                           initSlider();
+                        }, function(data, status) {
+                           console.error(status + ',' +data);
+                        });
+                  }
+                  //decreasing direction
+//                  else if
                   $('.iosSliderButtons .button').removeClass('selected');
                   $('.iosSliderButtons .button:eq(' + (currentSlideIndex) + ')').addClass('selected');
                }
